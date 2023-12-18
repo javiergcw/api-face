d87dcd6003bacd3101b96447695adbac5830c6fc
Javier Garcia Carrillo <50500251+javiergcw@users.noreply.github.com>
2023-12-18 16:06:26 -0500

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,126 @@
-import Image from 'next/image'
+"use client"
+import React, { useRef, useState, useEffect } from 'react';
+import * as faceapi from 'face-api.js';
 
-export default function Home() {
+const FaceDetection = () => {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+  const [isCameraActive, setIsCameraActive] = useState(false);
+  const [isDetecting, setIsDetecting] = useState(false);
+  const detectionInterval = useRef(null); // Correctamente declarado como useRef
+
+  const hatImg = useRef(new Image());
+
+  useEffect(() => {
+    // Pre-cargar la imagen del sombrero
+    hatImg.current = new Image();
+    hatImg.current.src = 'black-cowboy-hat-free.png';
+
+    if (isCameraActive) {
+      const displaySize = { width: videoRef.current.offsetWidth, height: videoRef.current.offsetHeight };
+      faceapi.matchDimensions(canvasRef.current, displaySize);
+    }
+  }, [isCameraActive]);
+
+  const loadModels = async () => {
+    const MODEL_URL = '/models';
+    await Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+      faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL)
+      // Carga otros modelos si es necesario
+    ]);
+  };
+
+  const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: {} })
+      .then(stream => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.onloadedmetadata = () => {
+          videoRef.current.play();
+          setIsCameraActive(true);
+          handleFaceDetection(); // Iniciar la detección de rostros automáticamente
+        };
+      })
+      .catch(err => console.error('Error al acceder a la cámara:', err));
+  };
+
+  const stopVideo = () => {
+    if (videoRef.current && videoRef.current.srcObject) {
+      videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+      setIsCameraActive(false);
+    }
+  };
+
+  const handleStart = () => {
+    loadModels().then(() => {
+      startVideo();
+      setIsCameraActive(true);
+    });
+  };
+
+  const handleFaceDetection = () => {
+    if (!isCameraActive || videoRef.current.readyState !== 4) return;
+
+    if (isDetecting) {
+      // Si ya se está detectando, limpiar el intervalo existente
+      clearInterval(detectionInterval.current);
+    } else {
+      setIsDetecting(true);
+    }
+    const options = new faceapi.TinyFaceDetectorOptions();
+    // Asegúrate de que el canvas tenga el mismo tamaño que el video
+    const displaySize = {
+      width: videoRef.current.videoWidth,
+      height: videoRef.current.videoHeight
+    };
+    // Ajusta el tamaño del canvas para que coincida con el tamaño del video
+    canvasRef.current.width = displaySize.width;
+    canvasRef.current.height = displaySize.height;
+    faceapi.matchDimensions(canvasRef.current, displaySize);
+
+    detectionInterval.current = setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(videoRef.current, options)
+        .withFaceLandmarks(); // Asegúrate de que cargaste este modelo
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      const ctx = canvasRef.current.getContext('2d');
+      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+      // Dibuja los landmarks y el sombrero en el canvas
+      resizedDetections.forEach(detection => {
+        // Dibuja los landmarks
+        faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+
+        // Aquí podrías agregar la lógica para dibujar el sombrero
+        // ...
+      });
+    }, 100);
+  };
+
+  const stopFaceDetection = () => {
+    if (detectionInterval.current) {
+      clearInterval(detectionInterval.current);
+      detectionInterval.current = null;
+      setIsDetecting(false);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      {!isCameraActive ? (
+        <button onClick={handleStart}>Activar Cámara</button>
+      ) : (
+        <>
+          <button onClick={stopVideo}>Desactivar Cámara</button>
+          <br />
+          <button onClick={isDetecting ? stopFaceDetection : handleFaceDetection}>
+            {isDetecting ? 'Detener Verificación de Rostros' : 'Verificar Rostros'}
+          </button>
+        </>
+      )}
+      <video ref={videoRef} autoPlay muted style={{ display: isCameraActive ? 'block' : 'none', position: 'absolute' }} />
+      <canvas ref={canvasRef} style={{ position: 'absolute' }} />
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default FaceDetection;
